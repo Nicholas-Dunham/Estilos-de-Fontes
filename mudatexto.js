22d9cfe2763bb99c068e6b91611854a2d638fbb2
Nicholas Waldstein Vidal Dunham <127153564+Nicholas-Dunham@users.noreply.github.com>
2023-03-20 02:24:29 +0000

    Redefinindo as caixas quando o input estiver vazio

--- mudatexto.js
+++ mudatexto.js
@@ -5,4 +5,12 @@ function changeTitle() {
   document.getElementsByClassName("title")[2].innerHTML = inputText; // Define o valor do input de texto como o novo conteúdo da tag h1
   document.getElementsByClassName("title")[3].innerHTML = inputText; // Define o valor do input de texto como o novo conteúdo da tag h1
   document.getElementsByClassName("title")[4].innerHTML = inputText; // Define o valor do input de texto como o novo conteúdo da tag h1
+
+  if(inputText==""){ //redefine o valor do input em cada caixa de texto
+    document.getElementsByClassName("title")[0].innerHTML = "Monospace";
+  document.getElementsByClassName("title")[1].innerHTML = "Sans-Serif"; 
+  document.getElementsByClassName("title")[2].innerHTML = "Serif"; 
+  document.getElementsByClassName("title")[3].innerHTML = "Handwriting"; 
+  document.getElementsByClassName("title")[4].innerHTML = "Display";
+  }
 }
